0cebd6f0975512afc339ea5714dbbcf994cbd70f
郭冬青 <guodongqing@guodongqingdeMac.local>
2017-03-31 23:04:01 -0700

    修改信息需要短信验证部分

--- IPMS/ChangeInformationViewController.swift
+++ IPMS/ChangeInformationViewController.swift
@@ -24,19 +24,28 @@ class ChangeInformationViewController: UIViewController {
         return changeTextField
     }()
     
-//    lazy var passwordText:UITextField = {
-//       let passwordText = UITextField()
-//        passwordText.placeholder = "请输入密码"
-//        passwordText.backgroundColor = UIColor.white
-//        passwordText.font = UIFont.systemFont(ofSize: 20)
-//        passwordText.layer.borderWidth = 1.0
-//        passwordText.borderStyle = UITextBorderStyle.roundedRect
-//        passwordText.layer.cornerRadius = 10
-//        passwordText.clearButtonMode = UITextFieldViewMode.always
-//        passwordText.isSecureTextEntry = true
-//        return passwordText
-//    }()
-//    
+    lazy var passwordText:UITextField = {
+       let passwordText = UITextField()
+        passwordText.placeholder = "输入手机验证码"
+        passwordText.backgroundColor = UIColor.white
+        passwordText.font = UIFont.systemFont(ofSize: 20)
+        passwordText.layer.borderWidth = 1.0
+        passwordText.borderStyle = UITextBorderStyle.roundedRect
+        passwordText.layer.cornerRadius = 10
+        passwordText.clearButtonMode = UITextFieldViewMode.always
+        passwordText.isSecureTextEntry = true
+        return passwordText
+    }()
+    lazy var getVerityButton:UIButton = {
+       let button = UIButton()
+        button.setTitle("获取验证码", for: .normal)
+        button.backgroundColor = UIColor.lightGray.withAlphaComponent(0.5)
+        button.setTitleColor(UIColor.black.withAlphaComponent(0.5), for: .normal)
+        button.titleLabel?.textAlignment = .center
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(ChangeInformationViewController.getVerityFromPhone(sender:)), for: .touchUpInside)
+        return button
+    }()
     
     var indexPath = Int()
     lazy var user:AVUser = {
@@ -57,7 +66,6 @@ class ChangeInformationViewController: UIViewController {
         self.navigationItem.rightBarButtonItem =  UIBarButtonItem(image: UIImage(named:"icon_save"), style: .plain, target: self, action: #selector(ChangeInformationViewController.save(sender:)))
         
 
-
         NotificationCenter.default.addObserver(self, selector: #selector(ChangeInformationViewController.keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(ChangeInformationViewController.keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
         
@@ -65,46 +73,101 @@ class ChangeInformationViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.setNavigationBarHidden(false, animated: false)
+        self.getVerityButton.isEnabled = true
     }
 
     func setupInterface(){
         self.view.addSubview(changeTextField)
-//        self.view.addSubview(passwordText)
         
+        if indexPath == 0{
+            
+        }else {
+            self.view.addSubview(passwordText)
+            self.view.addSubview(getVerityButton)
+            
+            passwordText.snp.makeConstraints { (make) in
+                make.leading.height.equalTo(changeTextField)
+                make.width.equalTo(self.view.bounds.width*2/3)
+                make.top.equalTo(changeTextField.snp.bottom).offset(5)
+            }
+            getVerityButton.snp.makeConstraints { (make) in
+                make.leading.equalTo(passwordText.snp.trailing).offset(5)
+                make.trailing.equalTo(self.view).offset(-8)
+                make.top.height.equalTo(passwordText)
+            }
+        }
+       
         changeTextField.snp.makeConstraints { (make) in
             make.leading.equalTo(self.view).offset(10)
             make.trailing.equalTo(self.view).offset(-10)
             make.top.equalTo(self.view).offset(78)
             make.height.equalTo(45)
         }
-//        passwordText.snp.makeConstraints { (make) in
-//            make.leading.trailing.height.equalTo(changeTextField)
-//            make.top.equalTo(changeTextField.snp.bottom).offset(5)
-//        }
-      
+        
     }
+    //获取验证码
+    func getVerityFromPhone(sender:UIButton){
+        
+        AVOSCloud.requestSmsCode(withPhoneNumber: self.user.mobilePhoneNumber!) { (success, error) in
+            if success {
+                sender.isEnabled = false
+                sender.backgroundColor = UIColor(red: 206/255, green: 189/255, blue: 176/255, alpha: 1)
+
+            }
+        }
+    }
+    //检验验证码
+    func veritySMSFromPhone(){
+        let verity = self.passwordText.text
+        
+        if verity != nil {
+            AVOSCloud.verifySmsCode(verity!, mobilePhoneNumber: self.user.mobilePhoneNumber!, callback: { (success, error) in
+                if success{
+                    //保存数据
+                    self.user.saveInBackground({ (success1, error1) in
+                        if success1 {
+                            self.addSuccessAlertView()
+                        }else{
+                            self.addFailedAlertView(type:2)
+                        }
+                    })
+                }
+                else {
+                    self.addFailedAlertView(type: 1)
+                }
+            })
+        }
+        else {
+            self.addFailedAlertView(type: 3)
+        }
+    }
+    
     func back(sender:UIBarButtonItem){
         self.navigationController?.popViewController(animated: true)
     }
+    
+    //保存按钮
     func save(sender:UIBarButtonItem){
+        
+        if self.changeTextField.text == "" {
+            self.addFailedAlertView(type: 3)
+            return
+        }
+        
         if indexPath == 0{
             user.username = changeTextField.text
             
         }else if indexPath == 1 {
             user["carNumber"] = Int(changeTextField.text!)
+            
+            
         }else if indexPath == 3 {
             user.mobilePhoneNumber = changeTextField.text
         }else if indexPath == 4 {
             user.email = changeTextField.text
         }
-        user.saveInBackground({ (success, error) in
-            if success {
-                self.addSuccessAlertView()
-            }else{
-                self.addFailedAlertView()
-            }
-        })
         
+        self.veritySMSFromPhone()
     }
     
     func addSuccessAlertView() {
@@ -115,10 +178,17 @@ class ChangeInformationViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    func addFailedAlertView() {
-        let alert = UIAlertController(title: "提示", message: "修改失败", preferredStyle: .alert)
+    func addFailedAlertView(type:Int) {
+        var message = "修改失败"
+        if type == 1{
+            message = "验证码错误"
+        }else if type == 3{
+            message = "请完善信息"
+        }
+        let alert = UIAlertController(title: "提示", message:message , preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "确定", style: .default, handler: { (cation) in
-             let _ = self.navigationController?.popViewController(animated: true)
+//             let _ = self.navigationController?.popViewController(animated: true)
+            
         }))
         present(alert, animated: true, completion: nil)
     }
